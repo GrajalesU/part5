465bf7fa20e81d88e5e1ee69d1193106f15cd584
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-02-06 16:14:50 -0500

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,17 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [showInfo, setShowInfo] = useState(false)
+  return (
+    < div className='blog'>
+      <p>{blog.title} {blog.author}</p>
+      {showInfo &&
+        <>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button> </p>
+        </>}
+      <button onClick={() => setShowInfo(!showInfo)}>{showInfo ? 'Cancel' : 'View'}</button>
+    </div >
+  )
+}
 
 export default Blog
