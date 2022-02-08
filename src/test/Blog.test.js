7f813e8fb8585a130cdb71639f2f12428432cf16
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-02-07 21:42:56 -0500

    5.14

--- src/test/Blog.test.js
+++ src/test/Blog.test.js
@@ -1,9 +1,9 @@
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from '../components/Blog'
 
 
-test('renders content', () => {
+test('renders blog', () => {
   const blog = {
     id: '1',
     title: 'Component testing is done with react-testing-library',
@@ -17,4 +17,22 @@ test('renders content', () => {
   expect(component.container).toHaveTextContent(`${blog.title} ${blog.author}`)
   expect(component.container).not.toHaveTextContent(blog.url)
   expect(component.container).not.toHaveTextContent(blog.likes)
+})
+
+test('renders expanded blog', () => {
+  const blog = {
+    id: '1',
+    title: 'Component testing is done with react-testing-library',
+    author: 'admin',
+    url: 'test.com',
+    likes: 0,
+    user: new Object('61fafd48697f3b584d1a4b75')
+  }
+
+  const component = render(<Blog blog={blog} />)
+  const button = component.getByText('View')
+  fireEvent.click(button)
+  expect(component.container).toHaveTextContent(blog.url)
+  expect(component.container).toHaveTextContent(blog.likes)
+
 })
